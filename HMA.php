01e41b3ee35400f1708db1dc6d70077800671445
Swargenze <djr6012@gmail.com>
2018-03-29 08:20:23 -0400

    Add files via upload

---
+++ HMA.php
@@ -0,0 +1,572 @@
+<?php 
+$dasar = '-';
+function GeneRate($qtd){ 
+$karakter = 'ABCDEFGHIJKLMOPQRSTUVXWYZ0123456789'; 
+$AnakPanti = strlen($karakter); 
+$AnakPanti--; 
+
+$Hash=NULL; 
+    for($x=0;$x<=$qtd;$x++){ 
+        $Posicao = rand(0,$AnakPanti); 
+        $Hash .= substr($karakter,$Posicao,1); 
+    } 
+
+return $Hash; 
+} 
+echo"<h1>CODE GENERATOR HMA</h1>";
+echo"<p > contact:https://www.facebook.com/dedicakqi.bebelek";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+
+
+
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+echo GeneRate(5); ; 
+
+echo "$dasar"; echo GeneRate(5); 
+
+echo "$dasar"; echo GeneRate(5); 
+echo "<br>";
+
+
+
+?>
